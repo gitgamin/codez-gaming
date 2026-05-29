80800f25d1be1776748926aa333422ee0a783b0b
gitgamin <msjyv864n8@privaterelay.appleid.com>
2026-05-29 17:34:30 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function updateClock(){
+
+const now = new Date();
+
+let h = now.getHours().toString().padStart(2,"0");
+let m = now.getMinutes().toString().padStart(2,"0");
+let s = now.getSeconds().toString().padStart(2,"0");
+
+document.getElementById("clock").innerHTML =
+`${h}:${m}:${s}`;
+
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
